f74656f436380f34a47c5cbdad83b7b8ad3acccb
CaliforniaLA <39907076+CaliforniaLA@users.noreply.github.com>
2018-10-31 11:26:25 +0800

    Add files via upload

---
+++ WorldMapTest.java
@@ -0,0 +1,25 @@
+package lifegame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WorldMapTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testWorldMap() {
+		fail("尚未实现");
+	}
+
+	@Test
+	public void testPaintComponentGraphics() {
+		fail("尚未实现");
+		
+	}
+
+}
